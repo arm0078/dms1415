61b537b1e6a6b659ee63431c82cb797d67418213
arm0078 <arm0078@alu.ubu.es>
2014-11-04 21:52:01 +0100

    Se completa algún método de la Fachada binaria

--- src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
+++ src/ubu/lsi/dms/agenda/persistencia/FachadaBin.java
@@ -3,6 +3,13 @@
  */
 package ubu.lsi.dms.agenda.persistencia;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import ubu.lsi.dms.agenda.modelo.Contacto;
@@ -10,50 +17,92 @@ import ubu.lsi.dms.agenda.modelo.Llamada;
 import ubu.lsi.dms.agenda.modelo.TipoContacto;
 
 /**
- * @author alumno
+ * @author
  *
  */
 public class FachadaBin implements FachadaPersistente {
-	
+
 	private static FachadaBin instance;
-	
+
+	private Collection<Contacto> contactos;
+	private Collection<TipoContacto> tipos;
+	private Collection<Llamada> llamadas;
+
+	@SuppressWarnings("unchecked")
 	private FachadaBin() {
+		
+		// Cargar objetos persistentes
+		try (ObjectInputStream inContactos = new ObjectInputStream(
+				new FileInputStream("res\\contactos.dat"));
+				ObjectInputStream inLlamadas = new ObjectInputStream(
+						new FileInputStream("res\\llamadas.dat"));
+				ObjectInputStream inTipos = new ObjectInputStream(
+						new FileInputStream("res\\tipos.dat"))) {
+			if (!isEmptyFile("res\\contactos.dat"))
+				contactos = (Collection<Contacto>) inContactos.readObject();
+			else
+				contactos = new ArrayList<>();
+			if (!isEmptyFile("res\\llamadas.dat"))
+				llamadas = (Collection<Llamada>) inLlamadas.readObject();
+			else
+				llamadas = new ArrayList<>();
+			if (!isEmptyFile("res\\tipos.dat")) 
+				tipos = (Collection<TipoContacto>) inTipos.readObject();
+			else
+				tipos = new ArrayList<>();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static FachadaBin getInstance() {
 		if (instance == null)
 			instance = new FachadaBin();
 		return instance;
 	}
-	
+
 	@Override
 	public void insertarLlamada(Llamada llamada) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void actualizarLlamada(Llamada llamada) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
-	public Collection<Llamada> consultarLlamadas() {
-		// TODO Auto-generated method stub
-		return null;
+	private boolean isEmptyFile(String nameFile) {
+		File file = new File(nameFile);
+		if (file.length() == 0)
+			return true;
+		return false;
 	}
 
 	@Override
 	public void insertarContacto(Contacto contacto) {
-		// TODO Auto-generated method stub
-		
+		if (isEmptyFile("res\\contactos.dat")) {
+			Collection<Contacto> contacto
+		}
+		try (ObjectOutputStream out = new ObjectOutputStream(
+				new FileOutputStream("res\\contactos.dat"))) {
+			out.writeObject();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void actualizarContacto(Contacto contacto) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -65,17 +114,32 @@ public class FachadaBin implements FachadaPersistente {
 	@Override
 	public void insertarTipoContacto(TipoContacto tipoContacto) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void actualizarTipoContacto(TipoContacto tipoContacto) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Collection<TipoContacto> consultarTiposContacto() {
+		return tipos;
+	}
+
+	@Override
+	public Collection<Llamada> consultarLlamadas(Contacto contacto) {
+		Collection<Llamada> llamadasContacto = new ArrayList<>();
+		for (Llamada llamada : llamadas){
+			if(llamada.getIdContacto() == contacto.getIdContacto())
+				llamadasContacto.add(llamada);
+		}
+		return llamadasContacto;
+	}
+
+	@Override
+	public Collection<Contacto> consultarContactos(String apellido) {
 		// TODO Auto-generated method stub
 		return null;
 	}
